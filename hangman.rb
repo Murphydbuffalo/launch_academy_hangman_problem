0d1855c02078179ba4ea80e31108c0a9cbaabe98
Dan Murphy <murphydbuffalo@gmail.com>
2014-05-14 17:13:14 -0400

    minor updates to hangman

--- hangman.rb
+++ hangman.rb
@@ -22,31 +22,27 @@ end
 def prompt(target, counter) #DRY's out user input and progress prompt
 	puts "Word to guess: #{target}  " #Create a and display "target" string of _ _ _ _ _'s
 	print "Guess a single letter or the entire word.  " #Prompt user for input
-	puts "#{counter} tries remaining!" #decrementing counter
+	puts "#{counter} tries remaining!\n" #decrementing counter
 end
 
-prompt(target, counter)
-guess = gets.chomp.upcase
-
 until counter == 0 || !target.include?("_") #Loop until no more tries remaining or user has guessed word
-	if guess.length === 1 #If single character
+	prompt(target, counter)
+	guess = gets.chomp.upcase
+	if guess.length == 1 #If single character
 		if previous_guesses.include?(guess)  #Did user already guess that letter?
-			puts "You've already guessed that letter!" #If yes, return to prompt
-			prompt(target, counter)
+			puts "You've already guessed that letter!\n" #If yes, return to prompt
 		elsif word.include?(guess) #If not, does word include guess?
-			puts "Good guess!"
+			puts "Good guess!\n"
 			until !word.include?(guess)
 			  position = word.index(guess)  #Index it, where in word is guess?
 			  target[position] = guess #Replace that position of word with guess using [ ]
 			  word[position] = "_" #Remove the letter from word for re-indexing
 			end 
 			add_guess(previous_guesses, guess) #ADD GUESS TO ALREADY GUESSED LETTERS
-			prompt(target, counter) #Return to prompt
 		else
-		    puts "Sorry, try again!"
+		    puts "Sorry, try again!\n"
 		    counter -= 1 #If not included, decrement counter, alert user
 			add_guess(previous_guesses, guess) #Add guess to already guessed
-			prompt(target, counter) #Return to prompt
 		end
 	elsif guess.length > 1  #If multicharacter, does guess equal word?
 		if guess == unaltered_word  
@@ -57,7 +53,6 @@ until counter == 0 || !target.include?("_") #Loop until no more tries remaining 
 		  break
 		end
 	end
-	guess = gets.chomp.upcase
 end
 
 congrats(target) if !target.include?("_")  #If target == word ...win
